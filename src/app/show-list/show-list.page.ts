6b7c8d3c7a36ce20d2628fe907337437364e8c01
jamessai4 <48791720+jamessai4@users.noreply.github.com>
2021-08-17 17:19:22 +0700

    .

--- src/app/show-list/show-list.page.ts
+++ src/app/show-list/show-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -8,11 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowListPage implements OnInit {
 
+  names = [];
+  contactname = "";
+  item: any;
 
-
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async addcontact() {
+
+    if (this.contactname == "") {
+      const alert = await this.alertController.create({
+        header: 'แจ้งเตือน',
+        message: 'กรุณากรอกข้อมูลให้ครบ',
+        buttons: ['ตกลง']
+      });
+
+      await alert.present();
+    } else
+
+      this.names.push(this.contactname);
+    this.contactname = "";
+  }
+
+  async removecontact(item) {
+
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'ตกลง',
+          handler: () => {
+            let index = this.names.indexOf(item);
+            this.names.splice(index);
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 
 }
